6f406185fd5587099b8bf58c4b0ea4bff6aa46d0
Seishiro Fukuda <108728183+seishiro0410@users.noreply.github.com>
2022-07-28 13:28:47 +0000

    3-7,3-8,3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,12 +5,15 @@ class ListsController < ApplicationController
   end
 
   def create
+    @list = List.new(list_params)
     #1&2 データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
     #3 データをデータベースに保存するためのsavaメソッド実行
-    list.save
+    if @list.save
     #4 詳細画面へリダイレク
-    redirect_to  list_path(list.id)
+      redirect_to  list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -29,6 +32,13 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])#データ（レコード）を1件取得
+    list.destroy#データ（レコードを削除
+    redirect_to '/lists' #投稿一覧画面へのリダイレクト
+
   end
 
   private
